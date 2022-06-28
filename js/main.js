9700bbd8c1026492caa192933c28f801f8b861ec
Coding for Christ <72089733+abbieruth@users.noreply.github.com>
2022-06-27 21:12:05 -0400

    commas and lowercase functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 document.querySelector('button').addEventListener('click', getActivity)
 
 function getActivity() {
-    let type = document.querySelector('input').value
+    let type = document.querySelector('input').value.toLowerCase();
     fetch(`http://www.boredapi.com/api/activity?type=${type}`)
     .then(res => res.json()) // parse as JSON
     .then(data => {
-        console.log(data)
-        document.querySelector('#activity').innerText = data.activity
-        document.querySelector('#participants').innerText = `${data.participants} participant(s) needed`
+        console.log(data);
+        document.querySelector('#activity').innerText = data.activity;
+        document.querySelector('#participants').innerText = `${data.participants} participant(s) needed`;
     })
     .catch(err => {
-        console.log(`error ${err}`)
+        console.log(`error ${err}`);
     })
 
 }
